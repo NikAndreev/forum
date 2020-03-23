5d2d7d2793bf97b4830b38ee33a4767b91cf7e5d
NikAndreev <57640150+NikAndreev@users.noreply.github.com>
2020-03-23 22:37:53 +0300

    Delete burger.js

--- js/burger.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.header-top__burger').click(function(event) {
-		$('.header-top__burger, .header-top__content').toggleClass('active');
-	});
-	$('.header-top__link').click(function(event) {
-		$('.header-top__burger, .header-top__content').removeClass('active');
-	});
-});
